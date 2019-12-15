f8ac2df95b71de7aca37644ab9f714b5de1ee51c
Maria Bilous <maria.bilous101@gmail.com>
2019-12-15 22:25:37 +0200

    basic Spring boot project created with Initializr

---
+++ src/main/java/ua/java_training/springbootinitialproject/SpringBootInitialProjectApplication.java
@@ -0,0 +1,13 @@
+package ua.java_training.springbootinitialproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootInitialProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootInitialProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ua/java_training/springbootinitialproject/SpringBootInitialProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.java_training.springbootinitialproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootInitialProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
